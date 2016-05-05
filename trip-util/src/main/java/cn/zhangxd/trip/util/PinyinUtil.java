8c151d3c8363a5085c0b79e61f6b151b6266e4d3
zhangxd1989 <zhangxd_2011sz@163.com>
2016-05-05 15:39:24 +0800

    增加图面处理,汉字拼音转换工具类

---
+++ trip-util/src/main/java/cn/zhangxd/trip/util/PinyinUtil.java
@@ -0,0 +1,123 @@
+package cn.zhangxd.trip.util;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 汉字转拼音工具类
+ */
+public class PinyinUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(PinyinUtil.class);
+
+    /**
+     * 将汉字转换为全拼
+     *
+     * @param src 源汉字
+     * @return String
+     */
+    public static String getPinYin(String src) {
+        char[] t1 = src.toCharArray();
+        String[] t2;
+        // 设置汉字拼音输出的格式
+        HanyuPinyinOutputFormat t3 = new HanyuPinyinOutputFormat();
+        t3.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        t3.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        t3.setVCharType(HanyuPinyinVCharType.WITH_V);
+        String t4 = "";
+        try {
+            for (char aT1 : t1) {
+                // 判断是否为汉字字符
+                if (Character.toString(aT1).matches("[\\u4E00-\\u9FA5]+")) {
+                    t2 = PinyinHelper.toHanyuPinyinStringArray(aT1, t3);// 将汉字的几种全拼都存到t2数组中
+                    t4 += t2[0];// 取出该汉字全拼的第一种读音并连接到字符串t4后
+                } else {
+                    // 如果不是汉字字符，直接取出字符并连接到字符串t4后
+                    t4 += Character.toString(aT1);
+                }
+            }
+        } catch (BadHanyuPinyinOutputFormatCombination e) {
+            logger.error("", e);
+        }
+        return t4;
+    }
+
+    /**
+     * 将汉字转换为全拼
+     *
+     * @param src 源汉字
+     * @return String
+     */
+    public static String getCamelPinYin(String src) {
+        char[] t1 = src.toCharArray();
+        String[] t2;
+        // 设置汉字拼音输出的格式
+        HanyuPinyinOutputFormat t3 = new HanyuPinyinOutputFormat();
+        t3.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        t3.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        t3.setVCharType(HanyuPinyinVCharType.WITH_V);
+        String t4 = "", t;
+        try {
+            for (char aT1 : t1) {
+                // 判断是否为汉字字符
+                if (Character.toString(aT1).matches("[\\u4E00-\\u9FA5]+")) {
+                    t2 = PinyinHelper.toHanyuPinyinStringArray(aT1, t3);// 将汉字的几种全拼都存到t2数组中
+                    t = t2[0];// 取出该汉字全拼的第一种读音并连接到字符串t4后
+                } else {
+                    // 如果不是汉字字符，直接取出字符并连接到字符串t4后
+                    t = Character.toString(aT1);
+                }
+                t = t.substring(0, 1).toUpperCase() + t.substring(1);
+                t4 += t;
+            }
+        } catch (BadHanyuPinyinOutputFormatCombination e) {
+            logger.error("", e);
+        }
+        return t4;
+    }
+
+    /**
+     * 提取每个汉字的首字母
+     *
+     * @param str 源汉字
+     * @return String
+     */
+    public static String getPinYinHeadChar(String str) {
+        String convert = "";
+        for (int j = 0; j < str.length(); j++) {
+            char word = str.charAt(j);
+            // 提取汉字的首字母
+            String[] pinyinArray = PinyinHelper.toHanyuPinyinStringArray(word);
+            if (pinyinArray != null) {
+                convert += pinyinArray[0].charAt(0);
+            } else {
+                convert += word;
+            }
+        }
+        return convert;
+    }
+
+    /**
+     * 提取每个汉字的大写首字母
+     *
+     * @param str 源汉字
+     * @return String
+     */
+    public static String getPinYinHeadUpperChar(String str) {
+        return getPinYinHeadChar(str).toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        String cnStr = "中华人民共和国";
+        System.out.println(getPinYin(cnStr));
+        System.out.println(getCamelPinYin(cnStr));
+        System.out.println(getPinYinHeadChar(cnStr));
+        System.out.println(getPinYinHeadUpperChar(cnStr));
+    }
+}
