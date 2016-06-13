a732ffc32a7ac3b8881269f131a1fb174053fa40
zhangxd1989 <zhangxd_2011sz@163.com>
2016-06-13 11:12:05 +0800

    去掉爬虫项目

--- trip-infrastructure/src/test/java/cn/zhangxd/trip/infrastructure/UserRepoTest.java
+++ trip-infrastructure/src/test/java/cn/zhangxd/trip/infrastructure/UserRepoTest.java
@@ -1,12 +1,7 @@
 package cn.zhangxd.trip.infrastructure;
 
-import cn.zhangxd.trip.Application;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-public class UserRepoTest {
-
-}
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = Application.class)
+//public class UserRepoTest {
+//
+//}
