4ae1c64b227819bafa860ba71191243ff515884d
zhangxd1989 <zhangxd_2011sz@163.com>
2016-07-15 21:42:13 +0800

    删除 custom oauth Exception

--- trip-client-mobile/src/main/java/cn/zhangxd/trip/client/mobile/security/exception/CustomOauthException.java
+++
@@ -1,8 +0,0 @@
-package cn.zhangxd.trip.client.mobile.security.exception;
-
-@JsonSerialize(using = CustomOauthExceptionSerializer.class)
-public class CustomOauthException extends OAuth2Exception {
-    public CustomOauthException(String msg) {
-        super(msg);
-    }
-}
