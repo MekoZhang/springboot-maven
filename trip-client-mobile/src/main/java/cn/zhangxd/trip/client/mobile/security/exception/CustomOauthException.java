74e83e4231614db452d7557371147b177e24968e
zhangxd1989 <zhangxd_2011sz@163.com>
2016-07-15 21:40:29 +0800

    custom oauth exception

---
+++ trip-client-mobile/src/main/java/cn/zhangxd/trip/client/mobile/security/exception/CustomOauthException.java
@@ -0,0 +1,8 @@
+package cn.zhangxd.trip.client.mobile.security.exception;
+
+@JsonSerialize(using = CustomOauthExceptionSerializer.class)
+public class CustomOauthException extends OAuth2Exception {
+    public CustomOauthException(String msg) {
+        super(msg);
+    }
+}
